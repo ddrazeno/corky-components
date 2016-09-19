6494d2f97d77e64fc4cf9fdba81bb766ddec729b
DavidAbram <agented2013@gmail.com>
2016-09-19 10:39:07 +0200

    html tests

--- gulpfile.js
+++ gulpfile.js
@@ -2,18 +2,57 @@ var gulp = require('gulp'),
     riot = require('riot'),
     jsesc = require('jsesc'),
     rename = require('gulp-rename'),
+    browserify = require('browserify'),
     ts = require('gulp-typescript'),
     merge = require('merge2'),
     rimraf = require('rimraf'),
-    through2 = require('through2');
+    through2 = require('through2'),
+    source = require('vinyl-source-stream'),
+    glob = require('glob'),
+    file = require('gulp-file'),
+    es = require('event-stream');
 
 gulp.task('build', ['clean', 'ts', 'tags']);
 
-gulp.task('clean', function(callback){
+gulp.task('clean', function (callback) {
     rimraf('./dist/**/', callback);
 });
 
-gulp.task('ts', ['clean'], function (){
+gulp.task('prepare-html-tests', function (done) {
+
+
+    glob('./test/**/*.dummy.js', function (err, files) {
+        if (err) done(err);
+
+        var hrefs = files.map(function(file){
+            var index = file.lastIndexOf('/');
+            var href = file.substring(0, index).replace('./test', '');
+            return `<a href="${href}">${href.replace('/', '')}</a>`
+        });
+
+        file('./test/index.html', `<!doctype html><html><body>${hrefs.join('</br>')}</body></html>`)
+        .pipe(gulp.dest('./'));
+
+         var tasks = files.map(function(entry) {
+            var index = entry.lastIndexOf('/');
+            
+            return browserify({ entries: [entry] })
+                .transform('babelify', {presets: ["es2015"]})
+                .bundle()
+                .pipe(source(entry))
+                .pipe(rename({
+                    extname: '.bundle.js'
+                }))
+                .pipe(file(entry.substring(0, index) + '/index.html', `<!doctype html><html><body><div id="app"></div><script src="${entry.replace('./test', '').replace('dummy', 'dummy.bundle')}"></script><script>Dummy();</script></script></body></html>`))
+                .pipe(gulp.dest('./'));
+            });
+            
+        es.merge(tasks).on('end', done);
+
+    });
+});
+
+gulp.task('ts', ['clean'], function () {
     var tsResult = gulp.src('./src/**/*.ts')
         .pipe(rename({
             basename: "element",
--- test/example/example.test.js
+++ test/example/example.test.js
@@ -1,48 +1,16 @@
 import { assert, expect } from 'chai';
-import { Example } from '../../dist/example/element'
-import { Service } from 'corky/flux/service';
-import { registerService, registerElement } from 'corky/tags/template';
-import * as tag from '../../dist/example/tag'
-import App from 'corky/app'
-import Flux from 'corky/flux'
+import Flux from 'corky/flux';
+import Dummy, {changeTitle} from './example.dummy';
 
 describe('Example', () => {
 
     var app;
-
-    var changeTitle = new Flux.Action('TITLE_CHANGED');
-
-    class DummyService extends Service {
-
-        constructor() {
-            super();
-            this.selector = (state) => (
-                {
-                    title: state.title
-                });
-            this.isContainer = true;
-        }
-    }
+    
+    
 
     beforeEach(() => {
 
-        var dummyReducer = new Flux.Reducer([
-            {
-                action: changeTitle,
-                reduce: (state, payload) => {
-                    state.title = payload;
-                }
-            }
-        ], { title: "Corky" })
-
-        app = new App(dummyReducer);
-
-        var tags = { a: tag };
-
-        registerElement(Example, 'a', tags)
-        registerService(Example, new DummyService())
-
-        app.init("#app", Example);
+        app = Dummy();
     });
 
         it('Renders', () => {
